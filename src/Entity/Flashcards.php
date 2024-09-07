d6cfef7036fa9b4be05b4dbf85e7870bd00eb364
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-09-07 18:44:56 +0200

    Add files via upload

---
+++ src/Entity/Flashcards.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FlashcardsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FlashcardsRepository::class)]
+class Flashcards
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $topic = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $answer = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTopic(): ?string
+    {
+        return $this->topic;
+    }
+
+    public function setTopic(string $topic): static
+    {
+        $this->topic = $topic;
+
+        return $this;
+    }
+
+    public function getAnswer(): ?string
+    {
+        return $this->answer;
+    }
+
+    public function setAnswer(string $answer): static
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+}
